51a4a0cdd5e316bdd716107a494a1cd6bafa35fb
leyla li <leylali@qq.com>
2019-08-17 17:00:23 -0700

    done with get and create tests

--- __tests__/models-singular/categories.test.js
+++ __tests__/models-singular/categories.test.js
@@ -2,15 +2,14 @@
 
 const supergoose = require('../supergoose.js');
 
-const Categories = require('../../models-singular/categories.js');
-// let categories = new Categories();
+const Categories = require('../../models-singular/categories');
+let categories = new Categories();
 
 describe('Categories Model (Singular)', () => {
 
   // How will you handle both the happy path and edge cases in these tests?
 
   it('can create() a new category', () => {
-    let categories = new Categories();
     const testCategories = {
       name: 'fruits',
       description: 'There will be some fruits here',
@@ -19,23 +18,84 @@ describe('Categories Model (Singular)', () => {
 
       .then(savedCategories=>{
         Object.keys(testCategories).forEach(key=>{
-          expect(savedCategories)[key].toEqual(testCategories[key]);
+          expect(savedCategories[key]).toEqual(testCategories[key]);
         })
       })
 
-      .catch(err=>console.log(err,'There is something wrong with the create test'));
+  //     .catch(err=>console.log(err,'There is something wrong with the create test'));
   });
 
   it('can get() a category', () => {
+    const testCategories = {
+      name: 'fruits',
+      description: 'There will be some fruits here',
+    };
+    return categories.create(testCategories)
+      
+      .then(savedCategories=>{
+        return categories.get(savedCategories._id);
+      })
+
+      .then(resolvedCategories=>{
+        Object.keys(testCategories).forEach(key=>{
+          expect(resolvedCategories[key]).toEqual(testCategories[key]);
+        });
+      })
+      // .catch(err=>{console.log(err)});
   });
 
   it('can get() all categories', () => {
+    const testCategories = {
+      name: 'fruits',
+      description: 'There will be some fruits here',
+    };
+    return categories.create(testCategories)
+      
+      .then((savedCategories)=>{
+        return categories.get('5d576e21ae7cf9402711da3f');
+      })
+
+      .then(resolvedCategories=>{
+        console.log('resolvedCate in the get all test', resolvedCategories);
+      //   Object.keys(testCategories).forEach(key=>{
+      //     expect(resolvedCategories[key]).toEqual(testCategories[key]);
+      // })
+      });
   });
 
   it('can update() a category', () => {
-  });
+    const testCategories = {
+      name: 'fruits',
+      description: 'There will be some fruits here',
+    };
+    return categories.create(testCategories)
 
-  it('can delete() a category', () => {
-  });
+      .then(savedCategories=>{
+        // console.log('got in savedCate', categories.update({id: savedCategories._id},{name: 'Peter'}));
+        return categories.update({id: savedCategories._id},{name: 'Banana'});
+      })
+
+      .then(updatedCategories=>{
+        // console.log('got in updated last then test', updatedCategories.find({name:'Peter'}));
+        expect (categories.find()).toEqual(updatedCategories.find());
+      })
+    });
+        
+    
+    it('can delete() a category', () => {
+      const testCategories = {
+            name: 'fruits',
+            description: 'There will be some fruits here',
+          };
+          return categories.create(testCategories)
+
+          .then(savedCategories=>{
+            return categories.delete(savedCategories._id);
+          })
+
+          .then(categoriesAfterDelete=>{
+            expect(categories.find({name: 'fruits'})).toEqual(false);
+          })
+        });
 
 });
--- models-singular/categories.js
+++ models-singular/categories.js
@@ -11,16 +11,24 @@ class Categories {
   }
 
   get(_id) {
+    console.log('got in get method');
     // Call the appropriate mongoose method to get
     // one or more records
     // If 1, return it as a plain object
     // If 2, return it as an object like this:
     // { count: ##, results: [{}, {}] }
     if(_id){
-      if(categoriesSchema.find({_id})){
-        console.log('found it');
+      console.log('got in get method and check id');
+
+      if(categoriesSchema.findOne({_id})){
+        // console.log('this is the id obj it found',categoriesSchema.findOne({_id}));
+        return categoriesSchema.findOne({_id});
+      }else{
+        console.log('this is the array of objs it found',categoriesSchema.find())
+        return {count: categoriesSchema.find().length, results: categoriesSchema.find()};
       }
-    }
+      }
+    return Promise.reject(new Error('Invalid Id'));
   }
 
   create(record) {
@@ -30,12 +38,19 @@ class Categories {
     return newRecord.save();
   }
 
-  update(_id, record) {
+  update(_id) {
     // Call the appropriate mongoose method to update a record
+    // console.log('This is in update funtion', _id,updateData);
+    // console.log('This is  after updated', categoriesSchema.findByIdAndUpdate({id: _id},updateData));
+    const updateObj = categoriesSchema.findOne({id:_id});
+    console.log(updateObj);
+    return updateObj.update(updateData);
+    // return categoriesSchema.findByIdAndUpdate({id: _id},record, {new:true});
   }
 
   delete(_id) {
     // Call the appropriate mongoose method to delete a record
+    return categoriesSchema.deleteOne({id: _id});
   }
 
 }
